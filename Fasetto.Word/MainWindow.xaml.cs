f28cae2affa4a87fb1be6bebc8b4d6846fe953c8
jungeunji <ejjung312@gmail.com>
2024-10-19 15:53:15 +0900

    Header, TItle

--- Fasetto.Word/MainWindow.xaml.cs
+++ Fasetto.Word/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Fasetto.Word.ViewModel;
 
 namespace Fasetto.Word
 {
@@ -19,6 +20,8 @@ namespace Fasetto.Word
         public MainWindow()
         {
             InitializeComponent();
+
+            this.DataContext = new WindowViewModel(this);
         }
     }
 }
---
+++ Fasetto.Word/ViewModel/WindowViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Fasetto.Word.ViewModel
+{
+    public class WindowViewModel : ObservableObject
+    {
+        private Window mWindow;
+
+        private int mOuterMarginSize = 10;
+        private int mWindowRadius = 10;
+
+        public int ResizeBorder { get; set; } = 6;
+
+        public Thickness ResizeBorderThickness { get { return new Thickness(ResizeBorder + OuterMarginSize); } }
+
+        public int OuterMarginSize 
+        {
+            get
+            {
+                return mWindow.WindowState == WindowState.Maximized ? 0 : mOuterMarginSize;
+            } 
+            set
+            {
+                mOuterMarginSize = value;
+            }
+        }
+
+        public Thickness OuterMarginSizeThickness { get { return new Thickness(OuterMarginSize); } } 
+
+        public int WindowRadius
+        {
+            get
+            {
+                return mWindow.WindowState == WindowState.Maximized ? 0 : mWindowRadius;
+            }
+            set
+            {
+                mWindowRadius = value;
+            }
+        }
+
+        public CornerRadius WindowCornerRadius { get { return new CornerRadius(WindowRadius); } }
+
+        public int TitleHeight { get; set; } = 42;
+
+        public GridLength TitleHeightGridLength { get { return new GridLength(TitleHeight + ResizeBorder); } }
+
+        public WindowViewModel(Window window)
+        {
+            mWindow = window;
+
+            mWindow.StateChanged += (sender, e) =>
+            {
+                Debug.Write("changed");
+            };
+        }
+    }
+}
