25843d332ffcd30fe8941789532c80c55d3d63bb
jungeunji <ejjung312@gmail.com>
2024-10-20 11:36:45 +0900

    finish

--- Fasetto.Word/ViewModel/WindowViewModel.cs
+++ Fasetto.Word/ViewModel/WindowViewModel.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Fasetto.Word.ViewModel
 {
@@ -16,9 +18,14 @@ namespace Fasetto.Word.ViewModel
         private int mOuterMarginSize = 10;
         private int mWindowRadius = 10;
 
+        public double WindowMinimumWidth { get; set; } = 400;
+        public double WindowMinimumHeight { get; set; } = 400;
+
         public int ResizeBorder { get; set; } = 6;
 
         public Thickness ResizeBorderThickness { get { return new Thickness(ResizeBorder + OuterMarginSize); } }
+
+        public Thickness InnerContentPadding { get { return new Thickness(ResizeBorder); } }
 
         public int OuterMarginSize 
         {
@@ -50,7 +57,15 @@ namespace Fasetto.Word.ViewModel
 
         public int TitleHeight { get; set; } = 42;
 
-        public GridLength TitleHeightGridLength { get { return new GridLength(TitleHeight + ResizeBorder); } }
+        public GridLength TitleHeightGridLength { get { return new GridLength(TitleHeight + ResizeBorder    ); } }
+
+        public RelayCommand MinimizeCommand { get; set; }
+
+        public RelayCommand MaximizeCommand { get; set; }
+
+        public RelayCommand CloseCommand { get; set; }
+
+        public RelayCommand MenuCommand { get; set; }
 
         public WindowViewModel(Window window)
         {
@@ -60,6 +75,32 @@ namespace Fasetto.Word.ViewModel
             {
                 Debug.Write("changed");
             };
+
+            // Create commands
+            MinimizeCommand = new RelayCommand(() =>
+            {
+                mWindow.WindowState = WindowState.Minimized;
+            });
+
+            MaximizeCommand = new RelayCommand(() =>
+            {
+                mWindow.WindowState ^= WindowState.Maximized;
+            });
+
+            CloseCommand = new RelayCommand(() => mWindow.Close());
+            MenuCommand = new RelayCommand(() => SystemCommands.ShowSystemMenu(mWindow, GetMousePosition()));
+
+            // Fix window resize issue
+            var resizer = new WindowResizer(mWindow);
+        }
+
+        private Point GetMousePosition()
+        {
+            // Position of the mouse relative to the window
+            var position = Mouse.GetPosition(mWindow);
+
+            // Add the window position so its a "ToScreen"
+            return new Point(position.X + mWindow.Left, position.Y + mWindow.Top);
         }
     }
 }
