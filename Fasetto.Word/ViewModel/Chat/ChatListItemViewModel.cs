929e2065928ea18ee3224e019d15320de3c67ade
정은지 <ejjung312@gmail.com>
2024-10-22 17:49:22 +0900

    ItemsControl Chat List & Design

---
+++ Fasetto.Word/ViewModel/Chat/ChatListItemViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Fasetto.Word
+{
+    public class ChatListItemViewModel : BaseViewModel
+    {
+        public string Name { get; set; }
+
+        public string Message { get; set; }
+
+        public string Initials { get; set; }
+
+        public string ProfilePictureRGB { get; set; }
+
+        public bool NewContentAvailable { get; set; }
+
+        public bool IsSelected { get; set; }
+    }
+}
---
+++ Fasetto.Word/ViewModel/Chat/Design/ChatListItemDesignModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fasetto.Word
+{
+    public class ChatListItemDesignModel : ChatListItemViewModel
+    {
+        public static ChatListItemDesignModel Instance { get; } = new ChatListItemDesignModel();
+
+        public ChatListItemDesignModel()
+        {
+            Initials = "AA";
+            Name = "Luke";
+            Message = "This chat app is awesome! I bet it will be fast too";
+            ProfilePictureRGB = "3099c5";
+        }
+    }
+}
